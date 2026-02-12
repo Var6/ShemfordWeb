8955410516926e6dc22229f153e8e69e0ff4ba77
Rishabh Ranjan <rk.remo12345@gmail.com>
2026-02-12 19:54:19 +0530

    build ready with bot

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -28,19 +28,19 @@ export default async function NotFound() {
           />
           <h1 className="text-5xl font-bold mb-4">Wait, What??</h1>
           <h2 className="text-2xl font-bold mb-8">
-            You&apos;re searching for{' '}
+            You&apos;re searching for {' '}
             <span className="text-danger">Something you shouldn&apos;t </span>?
             Eeuuu...
           </h2>
           <p className="text-lg mb-8">
-            That page doesn&apos;t exist, but don&apos;t worry, we&apos;ve got
-            you covered!
+            That page doesn&apos;t exist, but don&apos;t worry , we&apos;ve got
+            you covered !
           </p>
           <Link
             href="/"
             className="bg-orange-400 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded"
           >
-            Click me and I&apos; send you back to Home Page!
+            Click me and I&apos; send you back to Home Page !
           </Link>
         </div>
       </div>
